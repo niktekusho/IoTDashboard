6e5e9c9e0122a6eb3cf30f9712d9576751cc2783
Nicola <nicola.dalmaso@outlook.com>
2017-12-07 17:54:47 +0100

    Temperature Service: Fixed service settings

--- packages/temperature-service/src/settings/index.js
+++ packages/temperature-service/src/settings/index.js
@@ -1,7 +1,7 @@
 const db = require('./db');
 
 module.exports = {
-	host: process.env.MQTT_HOST || '192.168.1.129',
+	host: process.env.MQTT_HOST || 'localhost',
 	port: process.env.MQTT_PORT ? parseInt(process.env.MQTT_PORT, 10) : 1883,
 	db,
 };
