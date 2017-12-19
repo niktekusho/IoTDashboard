f3b3f9c269a721df9489c48dcbb71adfb2ffa3eb
niktekusho <nicola.dalmaso@outlook.com>
2017-12-19 14:29:57 +0100

    Temperature service: Changed default API port

--- packages/temperature-service/src/settings/index.js
+++ packages/temperature-service/src/settings/index.js
@@ -3,6 +3,6 @@ const db = require('./db');
 module.exports = {
 	host: process.env.MQTT_HOST || 'localhost',
 	port: process.env.MQTT_PORT ? parseInt(process.env.MQTT_PORT, 10) : 1883,
-	api_port: process.env.API_PORT || 3000,
+	api_port: process.env.API_PORT || 5000,
 	db,
 };
